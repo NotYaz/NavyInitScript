79100583225179b9e08fcdd441e5e4d166559fd7
Ivan Kozlov <k.john909@gmail.com>
2013-04-09 21:56:31 +0400

    Monit added

--- init.sh
+++ init.sh
@@ -2,7 +2,10 @@
 
 sudo add-apt-repository ppa:chris-lea/node.js  
 sudo apt-get update  
-sudo apt-get install build-essential nodejs git haproxy
+sudo apt-get install build-essential nodejs git upstart monit
+
+mkdir navyIO
+cd navyIO
 
 wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
 tar xzf redis-2.6.12.tar.gz
