8b3645f61590b99ac8e44be53591802bf06136ba
Ivan Kozlov <k.john909@gmail.com>
2013-04-09 21:03:26 +0400

    Initial commit

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:chris-lea/node.js  
+sudo apt-get update  
+sudo apt-get install build-essential nodejs git haproxy
+
+wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
+tar xzf redis-2.6.12.tar.gz
+cd redis-2.6.12
+make
+cd ..
+rm redis-2.6.12.tar.gz
