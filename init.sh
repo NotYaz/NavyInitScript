2511dd30e69c02601f913887abcde2051bee64d3
Ivan Kozlov <k.john909@gmail.com>
2013-05-23 19:47:08 +0400

    Haproxy ppa added

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:chris-lea/node.js  
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository ppa:nilya/haproxy-1.5
 sudo apt-get update  
 sudo apt-get install build-essential nodejs git haproxy
 
