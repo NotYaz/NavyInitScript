19d03527f425079466666a51b7c29a8651cb2866
Ivan Kozlov <k.john909@gmail.com>
2013-06-01 00:01:05 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,4 @@ sudo add-apt-repository ppa:chris-lea/node.js
 sudo add-apt-repository ppa:nilya/haproxy-1.5
 sudo add-apt-repository ppa:rwky/redis
 sudo apt-get update  
-sudo apt-get install build-essential nodejs git haproxy redis-server
-
+sudo apt-get install nodejs git haproxy redis-server #build-essential ??
